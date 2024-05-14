49ad84132f289e240c869b18181cebe9eb7a5b4d
root <root@DESKTOP-J9A0QIM>
2024-05-14 19:16:51 +0100

    commit

--- getData.js
+++ getData.js
@@ -1,7 +1,5 @@
 const fs = require("fs")
 const{GarminConnect } = require('garmin-connect')
-const yesterday = new Date()
-yesterday.setDate(yesterday.getDate()-1)
 //only 1 file der needed depnding on environment
 const fileDer = ""
 //const fileDer = "Documents/fitnessData/"
@@ -34,60 +32,4 @@ async function getData() {
         fs.writeFile("testGarminWeightExtract.json", saveWeightFile, (error) => { if(error) {console.error(error); throw error; } console.log("done correct"); }) 
         }
 
-function storeHr() {
-	//Load in existing data
-	hrFile = fs.readFileSync(fileDer+"hrAppend.json")
-	hrFile =  JSON.parse(hrFile)
-    console.log("Initial number of object in hrAppend: "+hrFile.length)
-
-    //Load in daily data
-    hrExtract = fs.readFileSync(fileDer+"testGarminHrExtract.json")
-	hrExtract =  JSON.parse(hrExtract)
-
-    delete hrExtract["heartRateValues"]
-	delete hrExtract["heartRateValueDescriptors"]
-
-    hrFile.push(hrExtract)
-	console.log(hrExtract)
-    console.log("Object in HR prior to save: "+hrFile.length)
-    hrFile = JSON.stringify(hrFile)
-    fs.writeFile(fileDer + "hrAppend.json", hrFile, (error) => { if(error) {console.error(error); throw error; } console.log("HR saved in root"); }) ;
-	fs.writeFile(fileDer + "archive/hrAppend"+yesterday+ ".json", hrFile, (error) => { if(error) {console.error(error); throw error; } console.log("HR saved in Archive"); })
-	fs.writeFile(fileDer + "uploadData/hrAppend.json", hrFile, (error) => { if(error) {console.error(error); throw error; } console.log("HR saved in upload"); })
-}
-
-function storeWeight(){
-    let entry = {}
-
-	weightFile = fs.readFileSync(fileDer+"weightAppend.json")
-	weightFile = JSON.parse(weightFile)
-    console.log("Initial number of object in weightAppend: "+weightFile.length)
-    //Load in daily data
-    weightExtract = fs.readFileSync(fileDer+"testGarminWeightExtract.json")
-    weightExtract =  JSON.parse(weightExtract)
-    //create variables and push 
-    if(weightExtract["dateWeightList"][0] == null){
-        console.log("No weight to record")
-        return
-    }
-    weight = weightExtract["dateWeightList"][0]["weight"]
-	weight = weight/1000
-	datePeriod = weightExtract["startDate"]
-	entry.weight = weight
-	entry.date = datePeriod
-    weightFile.push(entry)
-    console.log(weightFile)
-
-    console.log("Object in weight prior to save: "+weightFile.length)
-    weightFile = JSON.stringify(weightFile)
-    fs.writeFile(fileDer + "weightAppend.json", weightFile, (error) => { if(error) {console.error(error); throw error; } console.log("Weight saved in root"); }) ;
-	fs.writeFile(fileDer + "archive/weightAppend"+yesterday+ ".json", weightFile, (error) => { if(error) {console.error(error); throw error; } console.log("Weight saved in Archive"); })
-	fs.writeFile(fileDer + "uploadData/weightAppend.json", weightFile, (error) => { if(error) {console.error(error); throw error; } console.log("Weight saved in upload"); })
-}
-
-
 getData()
-
-//storeHr()
-
-//storeWeight()
